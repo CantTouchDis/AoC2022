ecc3193c80a94f74446f722d7cc5f4a35e54a0a9
Philipp Bausch <bauschp@informatik.uni-freiburg.de>
2022-12-10 09:20:04 +0100

    Fix Compiler Warning

--- util/Literal.hs
+++ util/Literal.hs
@@ -7,7 +7,7 @@ literally :: String -> Q Exp
 literally = return . LitE . StringL
 
 lit :: QuasiQuoter
-lit = QuasiQuoter { quoteExp = literally }
+lit = QuasiQuoter { quoteExp = literally, quotePat = undefined, quoteType = undefined, quoteDec = undefined}
 
 litFile :: QuasiQuoter
 litFile = quoteFile lit
